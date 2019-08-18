1693be3f519e05375e6c2eca95739a0a828a9f93
Shitanshu <shitanshu.19p@gmail.com>
2019-08-19 00:27:09 +0530

    Updating the contents of the helpers/ directory.

--- src/helpers/string-to-regex.ts
+++ src/helpers/string-to-regex.ts
@@ -1,17 +1,17 @@
-module.exports = (input) => {
+export default (input: string) => {
   // Validate input
   if (typeof input !== 'string') {
     throw new Error('Invalid input. Input must be a string')
   }
 
   // Parse input
-  const m = input.match(/(\/?)(.+)\1([a-z]*)/i)
+  const m: RegExpMatchArray | null = input.match(/(\/?)(.+)\1([a-z]*)/i)
 
   // Invalid flags
-  if (m[3] && !/^(?!.*?(.).*?\1)[gmixXsuUAJ]+$/.test(m[3])) {
+  if (m![3] && !/^(?!.*?(.).*?\1)[gmixXsuUAJ]+$/.test(m![3])) {
     return RegExp(input)
   }
 
   // Create the regular expression
-  return new RegExp(m[2], m[3])
+  return new RegExp(m![2], m![3])
 }
