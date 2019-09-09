50b9fe86762ada2b7e99568508c04857f8aef81c
Shitanshu <shitanshu.19p@gmail.com>
2019-09-10 01:02:53 +0530

    type checking schemas/data.ts

--- src/schemas/data.ts
+++ src/schemas/data.ts
@@ -1,6 +1,6 @@
-const Joi = require('@hapi/joi')
+import Joi from '@hapi/joi';
 
-module.exports = Joi.object()
+export default Joi.object()
   .meta({ label: 'Data' })
   .description('An object of data that will be used in the response template. This can include values from the webhook payload, information about the user, or values returned from previous actions in the same step.')
   .unknown()
